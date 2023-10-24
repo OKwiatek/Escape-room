e6e4b2e3f775787eed6fff2875c712ca8bdf58b6
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-10-24 09:40:49 +0200

    update

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,8 @@
 // Play button to puzzel 1 
-function p1() {
+const play = document.querySelector(".p1")
+play.addEventListener("click", myFucntion);
+
+function myFucntion{
     window.open("p1.html", "_self")
 }
+
